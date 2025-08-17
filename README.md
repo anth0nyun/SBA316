1ca6ddabc921bd9984a3b9a4f45843701a929875
anth0nyun <anthonyun72@gmail.com>
2025-08-17 19:44:52 -0400

    4

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## Guess The Number Webpage
 
+### How the game is played
+- The game picks a random secret number in a range (Easy 1–50, Normal 1–100, Hard 1–500).
+- You type a guess and press "Guess".
+- The page tells you if you should go higher or lower, until you get it right.
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,14 @@
+let target = 0;
+let max = 100;
+let over = false;
+
+
 let form = document.querySelector('#form');
 let guessInput = document.getElementById('guess');
 let diffSel = document.getElementById('diff');
 let rangeText = document.getElementById('rangeText');
+let newBtn = document.getElementById('new');
+
+
+
+
